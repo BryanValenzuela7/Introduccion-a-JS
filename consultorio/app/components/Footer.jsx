88afa444c7d252ddc91300e1a6b5970f18077658
BryanValenzuela7 <bryan3d2y@gmail.com>
2023-09-19 21:20:08 -0700

    Footer actualizado

--- consultorio/app/components/Footer.jsx
+++ consultorio/app/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Oswald } from 'next/font/google'
 const oswald = Oswald({ subsets: ['latin'], weight: '400' })
-function Footer({footerTitle}) {
+
+function Footer({ footerTitle }) {
   return (
-    <footer className={`bg-gray-800 p-4 ${oswald.className} mt-96`}>
-        <p className="text-white text-right">{footerTitle}</p>
+    <footer className={`bg-gray-800 p-4 ${oswald.className} fixed bottom-0 w-full`}>
+      <p className="text-white text-right">{footerTitle}</p>
     </footer>
   )
 }
